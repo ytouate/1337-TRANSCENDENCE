4d709d0d5366979e9a9a2906aa773f00ca562f60
Mohamed Korchi <korchimd@gmail.com>
2023-05-19 15:56:49 +0100

    using ConfigModule instead of dotenv

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
-require('dotenv').config();
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
+    constructor(private config: ConfigService) {
         super({
             datasources: {
                 db: {
-                    url: process.env.DATABASE_URL
+                    url: config.get('DATABASE_URL'),
                 }
             }
         });
