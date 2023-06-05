79c16d883a2320ea7260bd9cc5a3081d1233102f
Mohamed Korchi <korchimd@gmail.com>
2023-06-05 09:55:30 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,23 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { LobbyService } from './lobby/lobby.service';
+import { LobbyController } from './lobby/lobby.controller';
+import { LobbyModule } from './lobby/lobby.module';
+import { GateWayModule } from './gateway/gateway.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    AuthModule,
-    UserModule,
-    PrismaModule
-  ],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        AuthModule,
+        UserModule,
+        PrismaModule,
+        GateWayModule,
+        LobbyModule,
+    ],
+    providers: [LobbyService],
+    controllers: [LobbyController],
 })
-
 export class AppModule {}
