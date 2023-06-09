3f82bc0f5e0ca7f4f2f1cc1e1501076d58a4c945
Mohamed Korchi <korchimd@gmail.com>
2023-06-09 20:44:15 +0100

    move game logic to backend, game is playable, messy code

--- src/gateway/game.module.ts
+++ src/gateway/game.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GameGateWay } from './game.service';
+import { GameService } from 'src/game/game.service';
+import { GameModule } from 'src/game/game.module';
 
 @Module({
-    providers: [GameGateWay],
+    providers: [GameGateWay, GameService],
+    imports: [GameModule],
 })
 export class GateWayModule {}
--- src/gateway/game.service.ts
+++ src/gateway/game.service.ts
@@ -7,44 +7,60 @@ import {
     WebSocketGateway,
     WebSocketServer,
 } from '@nestjs/websockets';
-import { Lobby } from '@prisma/client';
 import { Server, Socket } from 'socket.io';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { User } from '@prisma/client';
-import { log } from 'console';
+import { GameService } from 'src/game/game.service';
+
+interface UserData {
+    socket: Socket;
+    username: string;
+    id: number;
+}
+
+export const WIN_CONDITION = 3;
+
+const BOARD_HEIGHT = 600;
+const BOARD_WIDTH = 900;
+
+const BALL_SIZE = 15;
+const BALL_SPEED_X = 8; // 10 originlly
+const BALL_SPEED_Y = 4;
+
+const PADDLE_WIDTH = 10;
+const PADDLE_HEIGHT = 100;
+const PADDLE_MARGIN = 10;
+
+interface PlayerPosition {
+    id: number;
+    username: string;
+    y: number;
+    score: number;
+}
+
+interface GamePosition {
+    player1: PlayerPosition;
+    player2: PlayerPosition;
+}
 
 @WebSocketGateway({ cors: true })
 export class GameGateWay
     implements OnGatewayConnection, OnModuleInit
 {
-    userSockets: Map<number, Socket>;
+    userSockets: Map<number, UserData>;
     queue: number[];
-    // rooms: Map<string, Set<string>>;
+    gamePlayerPosition: Map<number, GamePosition>;
 
     @WebSocketServer()
     server: Server;
 
-    constructor(private prisma: PrismaService) {
-        this.userSockets = new Map<number, Socket>();
+    constructor(
+        private prisma: PrismaService,
+        private gameService: GameService,
+    ) {
+        this.userSockets = new Map<number, UserData>();
         this.queue = [];
-        // this.rooms = new Map();
+        this.gamePlayerPosition = new Map();
     }
-
-    // joinRoom(socketId: string, roomId: string) {
-    //     if (!this.rooms.has(roomId))
-    //         this.rooms.set(roomId, new Set<string>());
-    //     this.rooms.get(roomId)?.add(socketId);
-    // }
-
-    // leaveRoom(socketId: string, roomId: string) {
-    //     if (this.rooms.has(roomId)) {
-    //         const sockets = this.rooms.get(roomId);
-    //         sockets?.delete(socketId);
-    //         if (sockets?.size === 0) {
-    //             this.rooms.delete(roomId);
-    //         }
-    //     }
-    // }
 
     onModuleInit() {
         this.server.on('connection', (socket) => {
@@ -57,8 +73,8 @@ export class GameGateWay
     }
 
     getUserIdBySocket(socket: Socket): number | undefined {
-        for (const [userId, client] of this.userSockets) {
-            if (client.id === socket.id) {
+        for (const [userId, userData] of this.userSockets) {
+            if (userData.socket.id === socket.id) {
                 return userId;
             }
         }
@@ -93,24 +109,28 @@ export class GameGateWay
             },
         });
 
+        const userData: UserData = {
+            socket: client,
+            username: user.username,
+            id: user.id,
+        };
+
         //means player exists and belongs to the lobby
         if (!this.userSockets.has(user.id)) {
             console.log(`Client with userId ${userId} connected`);
-            this.userSockets.set(user.id, client);
+
+            this.userSockets.set(user.id, userData);
         }
     }
 
     async matchPlayers(player1: number, player2: number) {
         // Get the sockets for the matched players
-        const socket1 = this.userSockets.get(player1);
-        const socket2 = this.userSockets.get(player2);
+        const userData1 = this.userSockets.get(player1);
+        const userData2 = this.userSockets.get(player2);
 
-        const game = await this.prisma.game.create({
-            data: {
-                players: {
-                    connect: [{ id: player1 }, { id: player2 }],
-                },
-            },
+        const game = await this.gameService.createGame({
+            player1: player1,
+            player2: player2,
         });
 
         if (!game) throw new Error('Failed to create the game');
@@ -118,48 +138,190 @@ export class GameGateWay
         const roomId = String(game.id);
         console.log('Joining Room:', roomId);
 
-        socket1.join(roomId);
-        socket2.join(roomId);
+        if (userData1) userData1.socket.join(roomId);
+        if (userData2) userData2.socket.join(roomId);
 
-        // this.server.to(roomId).emit('match_found', {
-        //     gameId: game.id,
-        //     opponentId: player2,
-        // });
+        if (!userData1 || !userData2) return;
 
-        socket1.emit('match_found', {
+        if (!userData1.socket || !userData2.socket) return;
+
+        userData1.socket.emit('match_found', {
             gameId: game.id,
-            opponentId: player2,
-            pos: 'left', // Custom data for player 1
+            opponent: userData2.username,
+            pos: 'left',
         });
 
-        socket2.emit('match_found', {
+        userData2.socket.emit('match_found', {
             gameId: game.id,
-            opponentId: player1,
-            pos: 'right', // Custom data for player 2
+            opponent: userData1.username,
+            pos: 'right',
         });
 
-        this.userSockets.delete(player1);
-        this.userSockets.delete(player2);
+        this.gamePlayerPosition.set(game.id, {
+            player1: {
+                id: userData1.id,
+                username: userData1.username,
+                y: BOARD_HEIGHT / 2 - PADDLE_HEIGHT / 2,
+                score: 0,
+            },
+            player2: {
+                id: userData2.id,
+                username: userData2.username,
+                y: BOARD_HEIGHT / 2 - PADDLE_HEIGHT / 2,
+                score: 0,
+            },
+        });
+
+        // this.userSockets.delete(player1); // still dont know what to do with this
+        // this.userSockets.delete(player2);
+
+        this.startGameLoop(
+            game.id,
+            userData1.socket,
+            userData2.socket,
+        );
     }
 
-    getSocketByID(id: string): Socket | undefined {
-        for (const [userId, socket] of this.userSockets) {
-            if (socket.id === id) {
-                return socket;
+    startGameLoop(roomId: number, socket1: Socket, socket2: Socket) {
+        var ball = {
+            x: BOARD_WIDTH / 2,
+            y: BOARD_HEIGHT / 2,
+            speedX: BALL_SPEED_X,
+            speedY: BALL_SPEED_Y,
+        };
+
+        const interval = setInterval(() => {
+            const gamePosition = this.gamePlayerPosition.get(roomId);
+
+            ball.x += ball.speedX;
+            ball.y += ball.speedY;
+
+            var reset = false;
+            var gameOver = false;
+
+            if (ball.x < PADDLE_MARGIN + BALL_SIZE + PADDLE_WIDTH) {
+                if (
+                    ball.y > gamePosition.player1.y &&
+                    ball.y < gamePosition.player1.y + PADDLE_HEIGHT
+                ) {
+                    ball.speedX *= -1;
+                    var deltaY =
+                        ball.y -
+                        (gamePosition.player1.y + PADDLE_HEIGHT / 2);
+                    ball.speedY = deltaY * 0.35;
+                } else if (ball.x < 0) {
+                    gamePosition.player2.score++;
+                    reset = true;
+                }
+            } else if (
+                ball.x >
+                BOARD_WIDTH - BALL_SIZE - PADDLE_WIDTH - PADDLE_MARGIN
+            ) {
+                if (
+                    ball.y > gamePosition.player2.y &&
+                    ball.y < gamePosition.player2.y + PADDLE_HEIGHT
+                ) {
+                    ball.speedX *= -1;
+                    var deltaY =
+                        ball.y -
+                        (gamePosition.player2.y + PADDLE_HEIGHT / 2);
+                    ball.speedY = deltaY * 0.35;
+                } else if (ball.x > BOARD_WIDTH) {
+                    gamePosition.player1.score++;
+                    reset = true;
+                }
             }
-        }
-        return undefined;
+
+            if (ball.y < 0) {
+                ball.speedY *= -1;
+            }
+            if (ball.y > BOARD_HEIGHT) {
+                ball.speedY *= -1;
+            }
+
+            if (reset) {
+                var data: PlayerPosition;
+                if (gamePosition.player1.score >= WIN_CONDITION) {
+                    gameOver = true;
+                    data = gamePosition.player1;
+                }
+                if (gamePosition.player2.score >= WIN_CONDITION) {
+                    gameOver = true;
+                    data = gamePosition.player2;
+                }
+                if (gameOver) {
+                    this.server.to(String(roomId)).emit('game_over', {
+                        data,
+                    });
+                    this.updateGameEnd(roomId, gamePosition);
+                    socket1.leave(String(roomId));
+                    socket2.leave(String(roomId));
+                    clearInterval(interval); // Stop the interval
+
+                    return;
+                }
+                ball.x = BOARD_WIDTH / 2;
+                ball.y = BOARD_HEIGHT / 2;
+                ball.speedX *= -1; // flip direction
+            }
+
+            this.server.to(String(roomId)).emit('game_update', {
+                ball: { x: ball.x, y: ball.y },
+                gamePosition,
+            });
+        }, 1000 / 60);
     }
+
+    updateGameEnd(gameId: number, gamePosition: GamePosition) {
+        try {
+            this.prisma.game.update({
+                where: {
+                    id: gameId,
+                },
+                data: {
+                    status: 'FINISHED',
+                    score1: gamePosition.player1.score,
+                    score2: gamePosition.player2.score,
+                },
+            });
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    //will use later
+    // updateGameScores(gameId: number, gamePosition: GamePosition) {
+    //     try {
+    //         this.prisma.game.update({
+    //             where: {
+    //                 id: gameId,
+    //             },
+    //             data: {
+    //                 score1: gamePosition.player1.score,
+    //                 score2: gamePosition.player2.score,
+    //             },
+    //         });
+    //     } catch (error) {
+    //         throw error;
+    //     }
+    // }
 
     @SubscribeMessage('mouseMove')
     mouseMove(
         @MessageBody() body: any,
         @ConnectedSocket() client: Socket,
     ) {
-        const { x, y, gameId } = body;
-        const roomId = String(gameId);
+        const { y, gameId, userId } = body;
+        // const userId = this.getUserIdBySocket(client);
 
-        client.to(roomId).emit('opponent_mousemove', { x, y });
+        const gamePositions = this.gamePlayerPosition.get(gameId);
+        if (gamePositions) {
+            if (userId == gamePositions.player1.id) {
+                gamePositions.player1.y = y;
+            } else if (userId == gamePositions.player2.id) {
+                gamePositions.player2.y = y;
+            }
+        }
     }
 
     @SubscribeMessage('queueUp')
