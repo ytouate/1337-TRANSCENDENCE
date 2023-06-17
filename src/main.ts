9fc3fb2f51a45de4891a3b8e1a08684a9fa8e586
Mohamed Korchi <korchimd@gmail.com>
2023-06-17 15:52:06 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { IoAdapter } from '@nestjs/platform-socket.io';
-import { ScheduleModule } from '@nestjs/schedule';
+
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -20,4 +20,3 @@ async function bootstrap() {
 
 bootstrap();
 
-ScheduleModule.forRoot();
