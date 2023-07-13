8985af754b34c1e1dbcb5c4094cb93d65e0d0a46
ilyasslefhail <ilefhail@student.1337.ma>
2023-07-13 18:48:43 +0100

    Delete backend/dist directory

--- backend/dist/profile/iprofile.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const InterfacePfoileServiceProvider = "InterfacePfoileServiceProvider";
-export interface IProfileService {
-    getProfile(req: any): any;
-    updatePhoto(req: any, filePath: any): any;
-    deletePhoto(req: any): any;
-    updateName(newUsername: any, req: any): any;
-}
