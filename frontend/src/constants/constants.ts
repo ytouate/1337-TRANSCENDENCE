f28537caeac0b829a571059acfdbbb4467207da0
Mohamed Korchi <korchimd@gmail.com>
2023-07-17 21:21:23 +0100

    constants interface added

---
+++ frontend/src/constants/constants.ts
@@ -0,0 +1,18 @@
+// Game
+export const WIN_CONDITION = 1;
+
+// export const BOARD_HEIGHT = 600;
+// export const BOARD_WIDTH = 900;
+export const BOARD_COLOR = 'black';
+
+export const BALL_SIZE = 15;
+export const BALL_COLOR = 'white';
+export const BALL_SPEED_X = 6;
+export const BALL_SPEED_Y = 4;
+
+export const PADDLE_WIDTH = 10;
+export const PADDLE_HEIGHT = 100;
+export const PADDLE_COLOR = 'white';
+export const PADDLE_MARGIN = 5;
+
+export const BACKEND_URL = 'http://10.32.124.200:3333';
