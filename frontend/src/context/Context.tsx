6ea62b4be0298633d82c505997603aabdf4692aa
Mohamed Korchi <korchimd@gmail.com>
2023-07-18 22:01:42 +0100

    commenting gameSocket

--- frontend/src/context/Context.tsx
+++ frontend/src/context/Context.tsx
@@ -1,10 +1,23 @@
-import { createContext } from "react";
-import socketIO from "socket.io-client";
-import Cookies from "js-cookie";
+import { createContext } from 'react';
+import socketIO from 'socket.io-client';
+import Cookies from 'js-cookie';
 export const authContext = createContext(false);
 export const userContext = createContext(null);
-export const socketContext = socketIO.connect("http://localhost:3000/notification", {
-    extraHeaders: {
-        Authorization: `Bearer ${Cookies.get("Token")}`,
+
+export const socketContext = socketIO.connect(
+    'http://localhost:3000/notification',
+    {
+        extraHeaders: {
+            Authorization: `Bearer ${Cookies.get('Token')}`,
+        },
     },
-});
+);
+
+// export const gameSocket = socketIO.connect(
+//     'http://localhost:3000/notification',
+//     {
+//         extraHeaders: {
+//             Authorization: `Bearer ${Cookies.get('Token')}`,
+//         },
+//     },
+// );
