15011f044094891dc97ea60338ecced461329202
Mohamed Korchi <korchimd@gmail.com>
2023-07-19 07:24:43 +0100

    ball velocity and vel/inc adjusted

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,8 +1,6 @@
 // AI Game
-export const WIN_CONDITION = 3;
+export const WIN_CONDITION = 1;
 
-// export const BOARD_HEIGHT = 600;
-// export const BOARD_WIDTH = 900;
 export const BOARD_COLOR = 'black';
 
 export const BALL_SIZE = 15;
@@ -15,11 +13,10 @@ export const PADDLE_HEIGHT = 100;
 export const PADDLE_COLOR = 'white';
 export const PADDLE_MARGIN = 5;
 
-
 export const PADDLE_MARGIN_RATIO = 0.0025;
 export const PADDLE_WIDTH_RATIO = 0.01;
 export const PADDLE_HEIGHT_RATIO = 0.1;
 export const BALL_SIZE_RATIO = 0.025;
 
-export const INITIAL_VELOCITY = 0.03;
-export const VELOCITY_INCREASE = 0.000007;
+export const INITIAL_VELOCITY = 0.02;
+export const VELOCITY_INCREASE = 0.000004;
